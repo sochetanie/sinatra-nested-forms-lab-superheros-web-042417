01bd7a1653d3e4a3a4357f54b55923aaae5b27ac
sochetanie <allagorik2901@gmail.com>
2017-05-17 15:04:57 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,15 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+    get '/' do
+    	erb :super_hero
+    end
+
+    post '/team' do
+    	@team_name = params["team"]["name"]
+    	@team_motto = params["team"]["motto"]
+    	@team_members = params["team"]["members"]
+    	erb :team
+    end
 
 end
